35b92148d1b7d0260e01b749df0886188c1c3418
TrabajoN1 <jaime.irribarra@virginiogomez.cl>
2021-10-19 23:22:16 -0300

    Vercion funcional 1.0

--- app/src/main/java/com/example/trabajon1_tallerdenuevastecnologias_jaimeirribarra/MainActivity.java
+++ app/src/main/java/com/example/trabajon1_tallerdenuevastecnologias_jaimeirribarra/MainActivity.java
@@ -3,24 +3,36 @@ package com.example.trabajon1_tallerdenuevastecnologias_jaimeirribarra;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.LabeledIntent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button Botoncalcular;
     private EditText Textnombre;
     private EditText TextCarrera;
+    private EditText nota1,nota2,nota3;
+    private double resulado;
+    private float fnota1,fnota2,fnota3,fresultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         TextCarrera = (EditText) findViewById(R.id.TextCarrera);
         Textnombre = (EditText) findViewById(R.id.TextNombre);
+        nota1 = (EditText) findViewById(R.id.TextNota1);
+        nota2 = (EditText) findViewById(R.id.TextNota2);
+        nota3 = (EditText) findViewById(R.id.TextNota3);
+
         Botoncalcular =(Button) findViewById(R.id.buttonCalcular);
         Intent intentCalc=new Intent(this,MainActivity2.class);
         Botoncalcular.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +40,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 intentCalc.putExtra("nombre",Textnombre.getText().toString());
                 intentCalc.putExtra("carrera",TextCarrera.getText().toString());
+
+
+                fnota1=Float.parseFloat(nota1.getText().toString());
+                fnota2=Float.parseFloat(nota2.getText().toString());
+                fnota3=Float.parseFloat(nota3.getText().toString());
+
+                fresultado=(fnota1+fnota2+fnota3)/3;
+
+
+
                 startActivity(intentCalc);
             }
         });
